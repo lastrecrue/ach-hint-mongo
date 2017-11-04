f7aeedd06d6795f7bcfa316f9b4478e7112caf3d
LAHMOURATE Achraf <achraf.lahmourate@orange.com>
2017-11-04 01:26:27 +0100

    todo date fromatter

--- src/main/java/ach/hin/data/MongoApplication.java
+++ src/main/java/ach/hin/data/MongoApplication.java
@@ -3,9 +3,11 @@ package ach.hin.data;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableMongoAuditing
+@EnableScheduling
 public class MongoApplication {
 
 	public static void main(String[] args) {

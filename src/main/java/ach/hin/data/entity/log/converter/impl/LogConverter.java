74991f4ae3286d2ae8a53feeb30dfbde6e7e2d0d
LAHMOURATE Achraf <achraf.lahmourate@orange.com>
2017-11-05 18:07:23 +0100

    fix no nFICME number

--- src/main/java/ach/hin/data/entity/log/converter/impl/LogConverter.java
+++ src/main/java/ach/hin/data/entity/log/converter/impl/LogConverter.java
@@ -17,12 +17,12 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class LogConverter {
 
-	final static Pattern TLC_REG_EX = getTlcRegex();
+	private final static Pattern TLC_REG_EX = getTlcRegex();
 
 	private static Pattern getTlcRegex() {
 		// "lordgun.org - - [07/Mar/2004:17:01:53 -0800] \"GET /razor.html HTTP/1.1\" 200 2869";
 
-		final String re1 = "(^[a-zA-Z0-9][a-zA-Z\\._\\-0-9]*[a-zA-Z0-9])";// tlc
+		final String re1 = "(^[a-zA-Z0-9][a-zA-Z0-9\\._\\-]*[a-zA-Z0-9])";// tlc
 		final String re2 = ".*?"; // Non-greedy match on filler
 		final String re3 = "(\\[.*?])"; // Square Braces 1
 		final String re4 = ".*?"; // Non-greedy match on filler
@@ -32,8 +32,7 @@ public class LogConverter {
 		final String re8 = ".*?"; // Non-greedy match on filler
 		final String re9 = "(\\d+|\\-)"; // Integer Number 2
 
-		final Pattern p = Pattern.compile(re1 + re2 + re3 + re4 + re5 + re6 + re7 + re8 + re9, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
-		return p;
+		return Pattern.compile(re1 + re2 + re3 + re4 + re5 + re6 + re7 + re8 + re9, Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
 	}
 
 	public LogAccess fromString(String ligne) {
